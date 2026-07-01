6db3d260a5e438fb1bf36d50929c365753fe8611
Aumulet-max <tanavat.bo.67@ubu.ac.th>
2026-07-01 09:57:50 +0700

    start and update

---
+++ Assets/Hello.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class Hello : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("Start");
+        //print("Start");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       Debug.Log("Update");
+       //print("Update");
+    }
+}
